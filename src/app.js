8b94a1d8a802f11d38d1d54b484fbf94f872fafa
shamanth.gs <shamanth.gs@gmail.com>
2021-03-24 06:52:09 -0600

    use puppeteer core and chrome aws lambda

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
-import { getExchangeRate } from './exchangeRate';
+import { getExchangeRate, getExchangeRateUsingPuppeteer } from './exchangeRate';
+import chromium from 'chrome-aws-lambda';
 import { sendSMS } from './smsClient';
 
-const handle = async () => {
-  try {
-    const exchangeRate = await getExchangeRate();
-    const smsMessage = `Currently 1 CAD$ = ${exchangeRate}`
-    await sendSMS({ message: smsMessage, phoneNumber: process.env.DEVPHONENO });
-  } catch (error) {
-    console.log('Error occured while delivering exchange rate', error);
-  }
-}
+export const handler = async (event, context, callback) => {
+  let result = 'result';
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
 
-handle();
+  try {
+    // const exchangeRate = await getExchangeRate();
+    const exchangeRate = await getExchangeRateUsingPuppeteer({
+      browser: browser,
+    });
+    const smsMessage = `Currently 1 CAD$ = ${exchangeRate}`;
+    // await sendSMS({ message: smsMessage, phoneNumber: process.env.DEVPHONENO });
+  } catch (error) {
+    // return callback(error);
+    console.log(error, 'Error occured while delivering exchange rate');
+  } finally {
+    if (browser !== null) {
+      console.log('attempting to close the pages and the browser');
+      const pages = await browser.pages();
+      await Promise.all(pages.map((page) => page.close()));
+      await browser.close();
+    }
+  }
+
+  // return callback(null, result);
+};
+
+handler();
--- src/exchangeRate.js
+++ src/exchangeRate.js
@@ -1,13 +1,16 @@
 import cheerio from 'cheerio';
-import axios from 'axios';
+// import axios from 'axios';
+import puppeteer from 'puppeteer';
 
 // const URL = 'https://www.remitly.com/ca/en/india/pricing';
 const URL = 'https://www.remitly.com/ca/en/india';
+const LOGINURL = 'https://www.remitly.com/ca/en/users/login';
 
-export const getPageWithExchangeRate = (url) => axios.get(URL);
+// export const getPageWithExchangeRate = (url) => axios.get(URL);
+export const getPageWithExchangeRate = (url) => '';
 
 export const getExchangeRate = async () => {
-  const {data: pageWithExchangeRate} = await getPageWithExchangeRate(URL);
+  const { data: pageWithExchangeRate } = await getPageWithExchangeRate(URL);
   console.log(pageWithExchangeRate);
   const $ = cheerio.load(pageWithExchangeRate);
   const exchangeRate = $('table tbody tr')
@@ -17,5 +20,25 @@ export const getExchangeRate = async () => {
     .find('div')
     .eq(0)
     .text();
+  return exchangeRate;
+};
+
+export const getExchangeRateUsingPuppeteer = async ({ browser }) => {
+  // const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(URL);
+
+  const pageWithExchangeRate = await page.content();
+
+  const $ = cheerio.load(pageWithExchangeRate);
+  const exchangeRate = $('table tbody tr')
+    .eq(1)
+    .find('td')
+    .eq(1)
+    .find('div')
+    .eq(0)
+    .text();
+  // await browser.close();
+  console.log('getExchangeRateUsingPuppeteer -> exchangeRate', exchangeRate);
   return exchangeRate;
 };
