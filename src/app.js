c3f1d955683bdab011d931b2585c5287d45a7b08
shamanth.gs <shamanth.gs@gmail.com>
2021-03-28 08:15:52 -0600

    use puppeteer core to get exchange rate

--- src/app.js
+++ src/app.js
@@ -1,37 +1,31 @@
-import { getExchangeRate, getExchangeRateUsingPuppeteer } from './exchangeRate';
+import { getExchangeRateUsingPuppeteer } from './exchangeRate';
 import chromium from 'chrome-aws-lambda';
 import { sendSMS } from './smsClient';
 
 export const handler = async (event, context, callback) => {
   let result = 'result';
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-
+  let browser = null;
   try {
-    // const exchangeRate = await getExchangeRate();
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
     const exchangeRate = await getExchangeRateUsingPuppeteer({
       browser: browser,
     });
     const smsMessage = `Currently 1 CAD$ = ${exchangeRate}`;
-    // await sendSMS({ message: smsMessage, phoneNumber: process.env.DEVPHONENO });
+    await sendSMS({ message: smsMessage, phoneNumber: process.env.DEVPHONENO });
+    console.log(smsMessage);
   } catch (error) {
-    // return callback(error);
     console.log(error, 'Error occured while delivering exchange rate');
   } finally {
     if (browser !== null) {
-      console.log('attempting to close the pages and the browser');
       const pages = await browser.pages();
       await Promise.all(pages.map((page) => page.close()));
       await browser.close();
     }
   }
-
-  // return callback(null, result);
 };
-
-handler();
--- src/exchangeRate.js
+++ src/exchangeRate.js
@@ -1,15 +1,23 @@
 import cheerio from 'cheerio';
-// import axios from 'axios';
-import puppeteer from 'puppeteer';
+import { remitlyUser } from './user';
 
-// const URL = 'https://www.remitly.com/ca/en/india/pricing';
 const URL = 'https://www.remitly.com/ca/en/india';
 const LOGINURL = 'https://www.remitly.com/ca/en/users/login';
+const LOGOUTURL = 'https://www.remitly.com/ca/en/users/logout';
 
-// export const getPageWithExchangeRate = (url) => axios.get(URL);
 export const getPageWithExchangeRate = (url) => '';
 
-export const getExchangeRate = async () => {
+export const isValidateExchangeRate = (domText) =>
+  domText.includes('CAD') && domText.includes('INR');
+
+export const parseExchangeRate = (domText) => {
+  if (!isValidateExchangeRate(domText)) {
+    return null;
+  }
+  return domText.split(' ')[2];
+};
+
+export const getExchangeRateUsingHttp = async () => {
   const { data: pageWithExchangeRate } = await getPageWithExchangeRate(URL);
   console.log(pageWithExchangeRate);
   const $ = cheerio.load(pageWithExchangeRate);
@@ -24,21 +32,27 @@ export const getExchangeRate = async () => {
 };
 
 export const getExchangeRateUsingPuppeteer = async ({ browser }) => {
-  // const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(URL);
+  await page.goto(LOGINURL);
+
+  await page.type('input#loginEmailField', remitlyUser.userName);
+  await page.type('input#loginPasswordField', remitlyUser.password);
+  await page.click('form#login button[type="submit"]');
+
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
   const pageWithExchangeRate = await page.content();
-
   const $ = cheerio.load(pageWithExchangeRate);
-  const exchangeRate = $('table tbody tr')
-    .eq(1)
-    .find('td')
-    .eq(1)
-    .find('div')
-    .eq(0)
-    .text();
-  // await browser.close();
-  console.log('getExchangeRateUsingPuppeteer -> exchangeRate', exchangeRate);
+
+  // const expressExchangeRateDetails = $(
+  //   'div[data-testid="product-EXPRESS"] .content > div'
+  // ).text();
+  const economyExchangeRateDetails = $(
+    'div[data-testid="product-BASIC"] .content > div'
+  ).text();
+
+  await page.goto(LOGOUTURL);
+  const exchangeRate = parseExchangeRate(economyExchangeRateDetails);
+
   return exchangeRate;
 };
